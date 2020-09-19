4b593cb2841be79d4dec6a66715f7cae7f90e9d6
takeuchikanji <wands.ur@ezweb.ne.jp>
2020-09-19 16:21:27 +0900

    basic

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.14.1"
 
 set :application, "kochi_ramune_market"
-set :repo_url, "git@example.com:takeuchikanji/kochi_ramune_market.git"
+set :repo_url, "git@github.com:takeuchikanji/kochi_ramune_market.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
