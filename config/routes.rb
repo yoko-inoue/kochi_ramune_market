92077cd0d11b498249287ae8f4e110f7ce882155
simeyoru <kemokumokemo@gmail.com>
2020-09-28 16:46:16 +0900

    9/28 update_rebase

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   #root 'items#index'
   root 'items#index'
-  resources :items, only: [:new]
+  resources :items, only: [:new, :create]
+  get  "/items/item_params" ,to: "items#item_params"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
