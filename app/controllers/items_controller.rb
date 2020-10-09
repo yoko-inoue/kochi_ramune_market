2340ab383829f7fc38fcb487297253d324211120
takeuchikanji <wands.ur@ezweb.ne.jp>
2020-10-09 19:47:11 +0900

    10/9 commit(signed_in?>>check-display_link)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,5 +6,10 @@ class ItemsController < ApplicationController
   end
 
   def buycheck
+    if signed_in?
+      @card = Card.get_card(current_user.card.customer_token) if current_user.card
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
