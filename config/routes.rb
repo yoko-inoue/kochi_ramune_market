146ec9a04285dc2c085f1dd8ac3007a19c7b8a77
simeyoru <kemokumokemo@gmail.com>
2020-10-19 13:54:30 +0900

    10/19 update

--- config/routes.rb
+++ config/routes.rb
@@ -39,3 +39,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html 
   resources :cards, only: [:index, :new, :create, :destroy] 
 end
+
