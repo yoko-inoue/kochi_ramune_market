ab46305a91943241f957f558163a75b0ac0fbbd4
takeuchikanji <wands.ur@ezweb.ne.jp>
2020-10-22 16:00:12 +0900

    10/22 nickname_length

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
+  validates :nickname, presence: true, length: { minimum: 2, maximum: 10 }
   has_one :profile
   has_one :sending_destination
 
